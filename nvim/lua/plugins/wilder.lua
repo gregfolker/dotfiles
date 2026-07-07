a55907e7e4aad978caa807bf3dd9947a7ed80e0a
Greg Folker <gfolker@amperecomputing.com>
2026-07-07 09:53:00 -0700

    feat(nvim): only open wilder menu when <tab> is pressed
    With enable_cmdline_enter = true (default), wilder is always
    showing when typing commands, so if <tab> is pressed it will
    cycle to the next item instead of the current. This makes it so pressing
    <tab> will open the menu for the first time on the first item, so using
    it as a quick autocompletion is easier.
    
    Signed-off-by: Greg Folker <gfolker@amperecomputing.com>

--- nvim/lua/plugins/wilder.lua
+++ nvim/lua/plugins/wilder.lua
@@ -2,6 +2,9 @@ return {
 	"gelguy/wilder.nvim",
 	opts = function()
 		local wilder = require("wilder")
-		wilder.setup({ modes = { ":", "/", "?" } })
+		wilder.setup({
+			modes = { ":", "/", "?" },
+			enable_cmdline_enter = false,
+		})
 	end,
 }
