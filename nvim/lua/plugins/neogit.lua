c16d350b0ed5a52ea78fc6ce84942961fa05e22e
Greg Folker <gfolker@amperecomputing.com>
2026-07-07 09:43:56 -0700

    feat(nvim): configure neogit
    Enable disable_insert_on_commit since I am almost always expecting to
    be in normal mode when a commit message opens
    
    Use unicode for graph style
    
    Show diff of staged changes in split view when writing a commit message.
    
    Signed-off-by: Greg Folker <gfolker@amperecomputing.com>

--- nvim/lua/plugins/neogit.lua
+++ nvim/lua/plugins/neogit.lua
@@ -10,4 +10,14 @@ return {
 	keys = {
 		{ "<leader>gg", "<cmd>Neogit<cr>", desc = "Show Neogit UI" },
 	},
+	opts = function()
+		require("neogit").setup({
+			disable_insert_on_commit = true,
+			graph_style = "unicode",
+			commit_editor = {
+				show_staged_diff = true,
+				staged_diff_split_kind = "vsplit",
+			},
+		})
+	end,
 }
