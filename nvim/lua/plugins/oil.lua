48577d274173ef6aa8b72deca4b801065cf6f40c
Greg Folker <gfolker@amperecomputing.com>
2026-07-10 14:01:57 -0700

    feat(nvim): show hidden files in Oil by default
    Show hidden files when looking at the file explorer in Oil by default
    
    Signed-off-by: Greg Folker <gfolker@amperecomputing.com>

--- nvim/lua/plugins/oil.lua
+++ nvim/lua/plugins/oil.lua
@@ -2,7 +2,11 @@ return {
 	"stevearc/oil.nvim",
 	---@module 'oil'
 	---@type oil.SetupOpts
-	opts = {},
+	opts = {
+		view_options = {
+			show_hidden = true, -- always show hidden files/folders
+		},
+	},
 	-- Optional dependencies
 	dependencies = { "nvim-tree/nvim-web-devicons" },
 	-- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
