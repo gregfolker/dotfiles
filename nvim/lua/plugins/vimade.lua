83b6977ead8b59bcfb3864e8d40ddc69a0e86a08
Greg Folker <gfolker@amperecomputing.com>
2026-07-15 13:22:05 -0700

    feat(nvim): make Vimade on-demand only
    There's too many situations where I don't actually want a split
    to be faded, for example the codediff-explorer, compiler-explorer, code
    outlines, etc., this makes it so Vimade can be activated on demand
    instead of always dimming splits.
    
    Signed-off-by: Greg Folker <gfolker@amperecomputing.com>

--- nvim/lua/plugins/vimade.lua
+++ nvim/lua/plugins/vimade.lua
@@ -3,17 +3,8 @@ return {
 	config = function()
 		require("vimade").setup({
 			recipe = { "default", { animate = true } },
-			fadelevel = 0.7,
-			exclude_filetypes = { "dashboard", "help", "calendar" },
-			blocklist = {
-				assembly = {
-					buf_opts = {
-						-- Assembly is almost always open in a split to view alongside source code,
-						-- so never dim those windows
-						ft = "asm",
-					},
-				},
-			},
+			fadelevel = 0.4,
+			ncmode = "focus",
 		})
 	end,
 }
