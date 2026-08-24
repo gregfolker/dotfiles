f05160b9eeac70a79abc63b4de53c1de8fc3504d
Greg Folker <gfolker@amperecomputing.com>
2026-08-24 12:50:00 -0700

    feat(nvim): add treesitter parsing for embedded code in mise TOML files
    From https://mise.jdx.dev/mise-cookbook/neovim.html, update neovim
    configuration to highlight syntax of embedded code inside of
    mise TOML files
    
    Signed-off-by: Greg Folker <gfolker@amperecomputing.com>

--- nvim/lua/plugins/tree-sitter-manager.lua
+++ nvim/lua/plugins/tree-sitter-manager.lua
@@ -19,6 +19,13 @@ return {
 				"lua",
 				"yaml",
 			},
+
+			-- Add the `is-mise?` predicate for syntax highlighting in mise files
+			require("vim.treesitter.query").add_predicate("is-mise?", function(_, _, bufnr, _)
+				local filepath = vim.api.nvim_buf_get_name(tonumber(bufnr) or 0)
+				local filename = vim.fn.fnamemodify(filepath, ":t")
+				return string.match(filename, ".*mise.*%.toml$") ~= nil
+			end, { force = true, all = false }),
 		})
 	end,
 }
